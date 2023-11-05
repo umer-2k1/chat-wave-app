3ec7387a056fa0a4eddef2012b5ef789ace0f2ad
Umer Memon <117743102+umermemon4648@users.noreply.github.com>
2023-11-05 14:08:39 +0500

    Update ChatWindow.tsx

--- src/screens/Chat/ChatWindow.tsx
+++ src/screens/Chat/ChatWindow.tsx
@@ -48,6 +48,16 @@ const ChatWindow = ({ navigation, route }: any) => {
           avatar: "https://placeimg.com/140/140/any",
         },
       },
+      {
+        _id: 2,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
     ]);
   }, []);
 
